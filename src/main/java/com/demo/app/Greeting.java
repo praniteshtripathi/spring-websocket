4a40fa3ebca99b0e038ba6474eb2930547af14c6
pranitesh tripathi <praniteshtripathi@gmail.com>
2021-06-08 20:59:30 +0530

    add

---
+++ src/main/java/com/demo/app/Greeting.java
@@ -0,0 +1,18 @@
+package com.demo.app;
+
+public class Greeting {
+
+	private String content;
+
+	public Greeting() {
+	}
+
+	public Greeting(String content) {
+		this.content = content;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+}
